c3e163de6a3e5fc83f9efb99b926b63cfc2af597
Siren-Blue <rowena.lily.lee@gmail.com>
2019-12-05 02:29:53 -0500

    parsing answer pages

--- Quora/crawler.py
+++ Quora/crawler.py
@@ -1,4 +1,5 @@
 from bs4 import BeautifulSoup
+import re
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
@@ -44,5 +45,33 @@ def ParserSearch():
         f_question.write(line+'\n')
     f_question.close()
 
+def CrawlReview():
+    f_name = open("file_name.txt","r")
+    f_url = open("url_list_m.txt","r")
+    questions = f_name.readlines()
+    urls = f_url.readlines()
+    for idx, url in enumerate(urls):
+        url = "http://www.quora.com" + url.strip('\n') + "?share=1"
+        try:
+            os.environ["webdriver.chrome.driver"] = "chromedriver"
+            browser = webdriver.Chrome()
+            browser.get(url)
+            src_updated = browser.page_source
+            src = ""
+            while src != src_updated:
+                time.sleep(3)
+                src = src_updated
+                browser.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+                src_updated = browser.page_source
+            html_source = browser.page_source
+            browser.quit()
+            f = open("test.txt", "w")
+            f.write(html_source)
+            f.close()
+        except:
+            pass
+        break
+
+
 if __name__ == "__main__":
-    ParserSearch()
+    CrawlReview()
