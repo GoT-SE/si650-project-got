a3aef214f978066d90311b7a32b40abe6e42630f
Tianyi <gty@umich.edu>
2019-12-04 22:08:20 -0500

    Add syn substitution

--- main/main.py
+++ main/main.py
@@ -1,24 +1,56 @@
+import json
+import nltk
+from nltk.corpus import stopwords
+from fuzzywuzzy import fuzz, process
+
+def init():
+    global stopwords
+    # nltk.download('stopwords')
+    stopwords = stopwords.words('english')
+
+# extracr a name from a sentence
+def extractNames(sentence):
+    tagged_sentences = nltk.pos_tag(nltk.word_tokenize(sentence))
+    
+    names = []
+    for chunk in nltk.ne_chunk(tagged_sentence):
+        if type(chunk) == nltk.tree.Tree:
+            if chunk.label() in ['GPE', 'PERSON']:
+                names.append(' '.join([c[0] for c in chunk]))
+    return names
+
 # take `text`
 # remove punctuation and lower the letters
 def cleanText(text):
+    global stopwords
     import re
     text = text.lower()
     text = re.sub("[^A-Za-z0-9\n]", " ", text)
     text = re.sub("\n", " ", text)
-
+    text = ' '.join([i for i in text.split() if i not in stopwords])
     return text
 
 # take `query`
 # replace the nicknames/first name with standard name
 def synSubstitution(query):
+    with open('../syn/wordcount-synonyms.json', 'r') as f:
+        syns = json.load(f)['synonyms']
+    
+    syns = [json.dumps(syn) for syn in syns]
+    names = extractNames(query)
+    for name in names:
+        best = process.extract(name, syns, scorer=fuzz.token_set_ratio, limit=1)[0]
+        if best[1] > 80:
+            query += ' ' + json.loads(best[0])['accepted']
+    
     return query
 
 # take `query`
 # first clean text
 # replace the nicknames/first name with standard name
 def queryPreprocess(query):
-    query = cleanText(query) # remove punctuation and lower the letters
     query = synSubstitution(query) # return standard person name
+    query = cleanText(query) # remove punctuation and lower the letters
     return query
 
 # take 'query' and return an episode set
@@ -38,7 +70,6 @@ def getSceneSet(episodeSet, query):
 # return `top_n` lines
 # sample return value: [('s01e01', 'Winter is Coming', '...')]
 def getScriptSet(query, top_n = 8):
-    from fuzzywuzzy import fuzz, process
     with open('../got/got.dat', 'r') as f:
         docs = f.read()
         docs = docs.split('\n')
@@ -52,7 +83,8 @@ def getScriptSet(query, top_n = 8):
     return res
 
 def main():
-    query = "you know nothing" # get the input
+    init()
+    query = "who's dead will never die" # get the input
     cmd = "script"
     if cmd == "scene": # search for scene
         query = queryPreprocess(query)
