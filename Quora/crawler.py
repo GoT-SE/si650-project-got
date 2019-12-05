5725d2aa36367aee44ae0f06aaf483af02476a2a
Siren-Blue <rowena.lily.lee@gmail.com>
2019-12-05 00:27:52 -0500

    question and urls parser

--- Quora/crawler.py
+++ Quora/crawler.py
@@ -1,4 +1,3 @@
-#%%
 from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.common.by import By
@@ -7,41 +6,43 @@ from selenium.webdriver.support.wait import WebDriverWait
 import time
 import os
 
-#%%
-# This cell crawl questions under the search
+def CrawlSearch():
+    questions_url = []
+    os.environ["webdriver.chrome.driver"] = "chromedriver"
+    browser = webdriver.Chrome()
+    browser.get("https://www.quora.com/search?q=game+of+throne+review")
 
-questions_url = []
-os.environ["webdriver.chrome.driver"] = "chromedriver"
-browser = webdriver.Chrome()
-browser.get("https://www.quora.com/search?q=game+of+throne+review")
-
-# load html of the search page
-src_updated = browser.page_source
-src = ""
-while src != src_updated:
-    time.sleep(3)
-    src = src_updated
-    browser.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+    # load html of the search page
     src_updated = browser.page_source
-html_source = browser.page_source
-f = open("search_result.html","w")
-f.write(html_source)
-f.close()
-browser.quit()
+    src = ""
+    while src != src_updated:
+        time.sleep(3)
+        src = src_updated
+        browser.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+        src_updated = browser.page_source
+    html_source = browser.page_source
+    f = open("search_result.html","w")
+    f.write(html_source)
+    f.close()
+    browser.quit()
 
-#%%
-# parse the html for questions and urls
-soup = BeautifulSoup(html_source)
-url_divs = soup.find_all("question_link")
-question_divs = soup.find_all("ui_qtext_rendered_qtext")
-print(url_divs)
+def ParserSearch():
+    f = open("search_result.html", "r")
+    html = f.read()
+    soup = BeautifulSoup(html, 'html5lib')
+    url_divs = soup.find_all("a", class_="question_link")
 
+    f_url = open("url_list.txt","w")
+    for element in url_divs:
+        f_url.write(element['href']+'\n')
+    f_url.close()
 
-    # browser.get("https://www.quora.com")
-    # input = browser.find_element_by_id("__w2_wbOSQ6e012_input")
-    # input.sendkeys(kw)
-    # input.sendkeys(Keys.ENTER)
-    # wait = WebDriverWait(browser, 10)
-    # wait.until(EC.presence_of_element_located((By.ID, 'content_left')))
-    # print(browser.current_url)
+    question_divs = soup.find_all("span", class_="ui_qtext_rendered_qtext")
+    f_question = open("question_list.txt", "w")
+    for element in question_divs:
+        line = element.get_text()
+        f_question.write(line+'\n')
+    f_question.close()
 
+if __name__ == "__main__":
+    ParserSearch()
